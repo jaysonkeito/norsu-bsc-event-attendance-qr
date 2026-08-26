03d2d8ea9c285f52ac81b42c0e565d7f784eb5a7
jaysonkeito <jayson.f.bsinfotech@gmail.com>
2026-08-26 10:45:45 +0800

    fix the script for creating the superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "YOUR_PROJECT.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qrproject.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
